e1db5a237464f349c982b21e0d740756c5b10975
Facundo Lopez <facua_2797@hotmail.com>
2023-02-27 23:06:19 -0300

    Primer escritura

---
+++ JAVA/acciones.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('boton').onclick = function() {
+    document.getElementById("demo").innerHTML='Soy una persona muy proactiva, con muchas ganas de trabajar, tanto individualmente como en equipo. Me destaco en la informática, mis experiencias en Microsoft e IBM lo avalan. El Desarrollo de Software es mi pasión y me encanta trabajar en ello, es por eso que estoy con mucho entusiasmo de vivir nuevos desafios.';
+}
+
+
+document.getElementById('boton_contrasena').addEventListener('click', function () {
+    document.res
+})
